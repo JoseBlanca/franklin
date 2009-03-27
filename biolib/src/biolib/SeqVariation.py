bdfddedb997dc338541a42f129fdf76502d8af19
Jose Blanca <jblanca@btc.upv.es>
2009-03-27 11:30:59 +0000

    Biopython cvs seqrecord removed

--- biolib/src/biolib/SeqVariation.py
+++ biolib/src/biolib/SeqVariation.py
@@ -3,79 +3,94 @@ Created on 2009 mar 25
 
 @author: peio
 '''
-#from biolib.contig import Contig, Location
+#from biolib.contig import Location
 
-class SeqVarConf(object):
+class _SeqVarConf(object):
+    '''This class contains some switches to configure to your needs
     '''
-    This class contains some switches to configure to your needs
-    '''
-    def __init__(self, min_num_of_reads = 1, only_snp = False):
-        ''' Here we initialize teh object with the configuration we want'''
-        self._min_num_of_reads = min_num_of_reads
-        self._only_snp         = only_snp
+    #No public methods at all, only some properties.
+    #we could use a namedtuple but it would be less clear.
+    #pylint: disable-msg=R0903
+    def __init__(self, min_num_of_reads=2, only_snp=False,
+                indel_char='-'):
+        ''' Here we initialize teh object with the configuration we want.
         
-    def min_num_reads(self):
-        ''' Returns the minimun number of reads for the second most
-        frecuent alele'''
-        return self._min_num_of_reads
-    
-    def only_snp(self):
-        ''' Returns if we are going to use only snps or not'''
-        return self._only_snp 
-        
+        This configuration will affect to all classes and functions from this
+        method.
+        min_num_of_reads is the number of times that an alleles has to be read
+        to be considered. This is used to ease the removal of some sequence
+        errors.
+        If only_snp is set to True the indels will be ignored.
+        keyword arguments:
+            min_num_of_reads: an int (default 2)
+            only_snp: a bool (default False)
+            indel_char: the character used for the inserts (default '-')
+        '''
+        self.min_num_of_reads = min_num_of_reads
+        self.only_snp         = only_snp
+        self.indel_char      = indel_char
+
+CONFIG = _SeqVarConf()
+
 class SeqVariation(object):
     '''
     This class is used to represent any kind of sequence variation in 
     and alignment. The variation can be a Snp or a InDel, or ...
     '''
 
-    def __init__(self, name, aleles, location = None, alignment = None):
+    def __init__(self, alleles, name=None, location=None, alignment=None):
         '''
         This class collect all the information about each secuence variation
-        in an alignment.Sequence variations could be SNPs or InDels. There is a
-        filter in the conf class to use only SNPs in this class.
-        Arguments:
-            name     - Name of the variations
-            location - Location of the Seq variation. Location class object
-            alignemt - Alignment where it procedes. It can be a COntig class 
-                       object
-            Aleles - . The aleles contains a dict with the reads and the aleles
-                       it have. example:
-                        aleles= {read1 = 'A', read2 = 'T',...}
-                        Keys are read names
-                        Value could be a string or a biopython Seq class object 
-        To initialize you only need the aleles information and the name.
+        in an alignment. Sequence variations could be SNPs or InDels. There is a
+        filter in the module configuration to use only SNPs in this class.
+        To initialize the only required information is the alleles.
+        Keyword arguments:
+            alleles - . The aleles contains a dict with the alleles and the
+                        number of times that it has been read.
+                        example:
+                            alleles= {'A':1, 'T':4}
+            name     - Name of the variation (default None)
+            location - Location of the SeqVariation. Location class object or
+                       int (default None).
+            alignemt - Alignment where it procedes. It can be a Contig class 
+                       object (default None).
         '''
-        
-        self._name      = name
-        self._aleles    = aleles
-        self._location  = location
-        self._alignment = alignment
-    
-    
+        self.name         = name
+        self._num_reads   = None  # the alleles dict with the alleles as keys
+        self._set_alleles(alleles)
+        self.location     = location
+        self.alignment    = alignment
     
     def __repr__(self):
         ''' It prints a readable SeqVariation information '''
         
-        pstring  = 'SequenceVariation: ' + str(self._name) + '\n'
-        if self._location:
-            pstring += self._location.__repr__() + '\n'
-        if self._alignment:
-            pstring += self._alignment.__repr__() + '\n'
-        pstring += self._aleles.__repr__()
+        pstring  = 'SequenceVariation: ' + str(self.name) + '\n'
+        if self.location:
+            pstring += self.location.__repr__() + '\n'
+        if self.alignment:
+            pstring += self.alignment.__repr__() + '\n'
+        pstring += self._num_reads.__repr__()
         return pstring
     
-    def aleles(self):
-        ''' It returns a dict with the aleles and  the reads it contains'''
-        return self._aleles
-    
-    def alignment(self):
-        '''It returns the contig it belong to, The reference '''
-        return self._alignment
-        
-    def name(self):
-        '''It returns the name of the SeqVariation '''
-        return self._name
+    def _get_alleles(self):
+        ''' It returns a dict with the aleles.'''
+        return self._num_reads
+    def _set_alleles(self, alleles):
+        ''' It sets the alleles dict.
+        It takes into account the min_number_of_reads and it remove the ones
+        that do not meet that criteria.
+        '''
+        for allele in alleles.keys():
+            #we remove the alleles with not enough reads
+            if alleles[allele] < CONFIG.min_num_of_reads:
+                del alleles[allele]
+                continue
+            #we remove the indels if we don't want them
+            if CONFIG.only_snp and allele == CONFIG.indel_char:
+                del alleles[allele]
+        self._num_reads = alleles
+
+    alleles = property(_get_alleles)
     
 def seq_var_in_alignment(contig):
     ''' We use this method to catch the Sequence variation from an
@@ -85,29 +100,6 @@ def seq_var_in_alignment(contig):
     ncols     = contig.ncols
     
     for loc_order in range(ncols):
-        colum_seq = contig[:,loc_order]
+        colum_seq = contig[:, loc_order]
         yield  colum_seq
          
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
--- biolib/src/test/seqvariation_test.py
+++ biolib/src/test/seqvariation_test.py
@@ -4,21 +4,61 @@ Created on 2009 mar 25
 @author: peio
 '''
 import unittest
-from biolib.SeqVariation import SeqVariation, SeqVarpConf, seq_var_in_alignment
-from biolib.contig import Contig, Location
+from biolib.SeqVariation import CONFIG, SeqVariation
+#from biolib.contig import Contig, Location
 #from test.test_utils import Seq, SeqWithQuality, SeqRecord, Seqmut
-from biolib.SeqRecord import SeqRecord
-class SeqVariationTest(unittest.TestCase):
-    ''' Here we will check if the SeqVariation module works as it should do
+
+class SeqVariationConfig(unittest.TestCase):
+    '''Here we will check if the SeqVariation module can be configured.
     '''
     @staticmethod
-    def test_seq_variation_init():
+    def test_config():
+        '''It checks that we can configure the module.'''
+        assert isinstance(CONFIG.min_num_of_reads, int)
+        assert isinstance(CONFIG.only_snp, bool)
+        indel_char = CONFIG.indel_char
+        assert isinstance(indel_char, str)
+        assert len(indel_char) == 1
+
+class SeqVariationTest(unittest.TestCase):
+    '''Here we will check if the SeqVariation module works as it should.
+
+    Is the support for snps and indels.
+    '''
+    @staticmethod
+    def test_init():
+        '''It tests the init'''
+        snp = SeqVariation(name='a snp', location=20,
+                           alignment='some_contig_instance',
+                           alleles={'A':2, 'T':3})
+        assert snp.name == 'a snp'
+        assert snp.location == 20
+        assert snp.alignment == 'some_contig_instance'
+        assert len(snp.alleles) == 2
+
+        #a simple init
+        snp = SeqVariation(alleles={'A':2, 'T':3})
+        assert len(snp.alleles) == 2
+
+        #some alleles are removed due to the min_number_of_reads
+        CONFIG.min_num_of_reads = 3
+        snp = SeqVariation(alleles={'A':2, 'T':3})
+        assert len(snp.alleles) == 1
+
+        #some alleles are removed due to only_snps
+        CONFIG.only_snp = True
+        indel = CONFIG.indel_char
+        snp = SeqVariation(alleles={'A':3, indel:3})
+        assert len(snp.alleles) == 1
+
+    @staticmethod
+    def test_seq_variation_generator():
         '''It checks if the '''
        
-        contig = Contig(SeqRecord('aa', name='seq1'),
-                        SeqRecord('at',name='seq2'))
-        for seq_var in seq_var_in_alignment(contig):
-            print type(seq_var)
+        #contig = Contig(SeqRecord('aa', name='seq1'),
+        #                SeqRecord('at',name='seq2'))
+        #for seq_var in seq_var_in_alignment(contig):
+            #    print type(seq_var)
 
 
 if __name__ == "__main__":
