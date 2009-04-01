cff92e8febde99effbbbbddac2192762326e9247
Jose Blanca <jblanca@btc.upv.es>
2009-04-01 11:01:33 +0000

    setup.py added

---
+++ biolib/src/setup.py
@@ -0,0 +1,20 @@
+'''
+Created on 25/03/2009
+
+@author: jose blanca
+'''
+
+from setuptools import setup
+setup(
+    # basic package data
+    name = "biolib",
+    version = "0.0.1",
+    author='Jose Blanca, Peio Ziarsolo',
+    author_email='jblanca@btc.upv.es',
+    description='Some genomics related classes',
+    # package structure
+    packages=['biolib'],
+    package_dir={'':'.'},
+    requires=['Nose'],
+    test_suite='nose.collector'
+)
