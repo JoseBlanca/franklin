af7d8b7339706c1763552a3a2d7674da6168e2d1
Peio Ziarsolo <pziarsolo@upvnet.upv.es>
2009-10-08 10:23:12 +0000

    put words in repair or forget. and all its dependencies

--- biolib/src/repair_or_forget/words.py
+++ biolib/src/repair_or_forget/words.py
@@ -20,8 +20,8 @@ Created on 03/09/2009
 @author: jose
 '''
 import array
-from biolib.collections_ import SparseVector
 from biolib.rbtree import RBDict
+import pysparse
 
 def cluster_seqs_by_words(seqs, wsize, count=False):
     '''It looks for sequences that share at least one word'''
@@ -182,3 +182,92 @@ def filter_low_abundant_words(word_abundance, min_abundance, wsize):
         return int(abundance1[1] - abundance2[1])
     filtered_words = sorted(filtered_words, cmp)
     return filtered_words
+
+
+class SparseVector(object):
+    'A sparse vector collection to hold nearly empty vectors.'
+    def __init__(self, nelements, size_hint=None, store_non_int=False):
+        '''It initializes the vector.
+
+        We require the number of elements that the vector should hold.
+        size_hint is the number of estimated elements that the vector should
+        hold at the end. If given expensive memory reallocation will be saved.
+        '''
+        self._size = nelements
+        if size_hint:
+            self._spv = pysparse.spmatrix.ll_mat(nelements, 1, size_hint)
+        else:
+            self._spv = pysparse.spmatrix.ll_mat(nelements, 1)
+        if store_non_int:
+            self._cargo = []
+            self._cargo.append(None)   #we dont' use the 0 as cargo index
+        else:
+            self._cargo = None
+        self._position = 0
+
+    def __setitem__(self, index, value):
+        'It sets one item in the vector'
+        cargo = self._cargo
+        if cargo is None:
+            self._spv[index, 0] = value
+        else:
+            #was an item already stored in that position?
+            cargo_index = self._spv[index, 0]
+            if cargo_index:
+                self._cargo[cargo_index] = value
+            else:
+                self._cargo.append(value)
+                cargo_index = len(self._cargo) - 1
+                self._spv[index, 0] = cargo_index
+
+    def __getitem__(self, index):
+        'It returns one element'
+        cargo = self._cargo
+        if cargo is None:
+            return self._spv[index, 0]
+        else:
+            return cargo[int(self._spv[index, 0])]
+    def __iter__(self):
+        'The iterable protocol'
+        self._position = 0
+        return self
+
+    def next(self):
+        'It returns the next item'
+        position = self._position
+        if position < self._size:
+            value = self[self._position]
+            self._position += 1
+            return value
+        else:
+            raise StopIteration
+
+    def __len__(self):
+        'It returns the number of elements'
+        return self._size
+
+    def __str__(self):
+        'It prints the vector'
+        toprint = '['
+        for item in self:
+            toprint += str(item)
+            toprint += ', '
+        toprint += ']'
+        return toprint
+
+    def non_empty_items(self):
+        'It return a iterator with non empty items'
+        keys = self._spv.keys()[0]
+        last_item = len(keys) - 1
+        for index, key in enumerate(keys):
+            if self._cargo:
+                yield key, self._cargo[int(self._spv[key, 0])]
+            else:
+                yield key, self._spv[key, 0]
+            if index == last_item:
+                raise StopIteration
+
+    def non_empty_values(self):
+        'It return a iterator with non empty items'
+        for item in self.non_empty_items():
+            yield item[1]
--- biolib/src/scripts/seqvar/run_bwa.py
+++ biolib/src/scripts/seqvar/run_bwa.py
@@ -24,10 +24,44 @@ Created on 08/10/2009
 
 @author: peio
 '''
+from optparse import OptionParser
+from biolib.biolib_seqio_utils import guess_seq_file_format
+def parse_options():
+    'It parses the command line arguments'
+    parser = OptionParser('usage: %prog -i sam_pileup, -o req_pos -p pipeline')
+    parser.add_option('-s', '--seqs', dest='seqfile', help='Sequences file')
+    parser.add_option('-f', '--format', dest='format',
+                      help='Sequences file format')
+    parser.add_option('-r', '--references', dest='reffiles',
+                      help='reference seq file')
+    parser.add_option('-i', '--refdb', dest='refdb', help='reference index')
+    parser.add_option('-b', '--bamfile', dest='samfile', help='bam output file')
 
+    return parser
+def set_parameters():
+    '''It sets the parameters for the script.'''
 
+    parser  = parse_options()
+    options = parser.parse_args()[0]
 
+    if options.seqfile is None:
+        parser.error('seqfile is mandatory')
+    else:
+        seqfhand = open(options.seqfile)
 
+    if options.reffile is None:
+        parser.error('file is mandatory')
+    else:
+        reffhand = open(options.seqfile)
+
+    if options.format is None:
+        file_format = guess_seq_file_format(seqfhand)
+    else:
+        file_format = options.format
+
+    refdb = options.refdb
+    if options.bamfile 
+    
 
 
 if __name__ == '__main__':
