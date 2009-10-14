74d942339bd6916b2b6171b179f43badde767f80
Peio Ziarsolo <pziarsolo@upvnet.upv.es>
2009-10-14 14:41:14 +0000

    added SeqIO.convert from biopython 1.52

--- biolib/src/scripts/seqio.py
+++ biolib/src/scripts/seqio.py
@@ -3,6 +3,7 @@
 
 from optparse import OptionParser
 from biolib.biolib_seqio_utils import seqs_in_file, write_seqs_in_file
+from Bio import SeqIO
 
 def parse_options():
     'It parses the command line arguments'
@@ -19,33 +20,46 @@ def parse_options():
                       help='output quality file')
     parser.add_option('-e', '--outformat', dest="outformat",
                       help='output file format')
-    return parser.parse_args()
+    return parser
+
+def set_parameters():
+    'Set parameters'
+    # Set parameters
+    parser  = parse_options()
+    options = parser.parse_args()[0]
+
+    infhand_seq = open(options.inseqfile)
+    if options.inqualfile:
+        infhand_qual = open(options.inqualfile)
+    else:
+        infhand_qual = None
+
+    format_in = options.informat
+
+    outfhand_seq = open(options.outseqfile, 'w')
+    if options.outqualfile:
+        outfhand_qual = open(options.outqualfile, 'w')
+    else:
+        outfhand_qual = None
+    format_out = options.outformat
+
+    return (infhand_seq, infhand_qual, format_in, outfhand_seq, outfhand_qual,
+            format_out)
+
 
 def main():
     'The main function'
-    # Set parameters
-    opts = parse_options()[0]
-    in_seq_fhand = open(opts.inseqfile)
-    if opts.inqualfile:
-        in_qual_fhand = open(opts.inqualfile)
-    else:
-        in_qual_fhand = None
-    in_format = opts.informat
+    (infhand_seq, infhand_qual, format_in, outfhand_seq, outfhand_qual,
+     format_out) = set_parameters()
 
-    out_seq_fhand = open(opts.outseqfile, 'w')
-    if opts.outqualfile:
-        out_qual_fhand = open(opts.outqualfile, 'w')
+    if  infhand_qual is not None or outfhand_qual is not None:
+        seqs = seqs_in_file(seq_fhand=infhand_seq, qual_fhand=infhand_qual,
+                        format=format_in, create_seqrecord=True)
+        write_seqs_in_file(seqs, seq_fhand=outfhand_seq,
+                           qual_fhand=outfhand_qual,format=format_out)
     else:
-        out_qual_fhand = None
-    out_format = opts.outformat
+        SeqIO.convert(infhand_seq, format_in, outfhand_seq, format_out)
 
-    seqs = seqs_in_file(seq_fhand=in_seq_fhand,
-                        qual_fhand=in_qual_fhand,
-                        format=in_format,
-                        create_seqrecord=True)
-    write_seqs_in_file(seqs, seq_fhand=out_seq_fhand,
-                       qual_fhand=out_qual_fhand,
-                       format=out_format)
 
 if __name__ == '__main__':
     main()
