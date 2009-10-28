29cff93e2248bc9c3f7223b968da590710e58ef3
Jose Blanca <jblanca@btc.upv.es>
2009-10-28 08:28:37 +0000

    svn tests moved to test/snv

---
+++ biolib/src/test/snv/seqvariation_test.py
@@ -0,0 +1,214 @@
+'''
+Created on 2009 mar 25
+
+@author: peio
+'''
+
+# Copyright 2009 Jose Blanca, Peio Ziarsolo, COMAV-Univ. Politecnica Valencia
+# This file is part of biolib.
+# biolib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# biolib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with biolib. If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+from StringIO import StringIO
+from biolib.seqvar.seqvariation import (pic, cap_enzymes, SNP,
+                                        INSERTION, DELETION, INVARIANT, INDEL,
+                                        COMPLEX, Snv, snvs_in_file,
+                                        major_allele_frequency,
+                                        reference_variability,
+                                        calculate_kind)
+from biolib.seqs import SeqWithQuality
+
+class SnvTest(unittest.TestCase):
+    '''Here we will check if the Snv module works as it should.'''
+    @staticmethod
+    def test_init():
+        '''It tests the init'''
+        per_lib_info = [{'alleles':[{'allele':'A', 'reads':3, 'kind':SNP},
+                                  {'allele':'T', 'reads':4, 'kind':INVARIANT}]}]
+
+        snv = Snv(per_lib_info=per_lib_info, reference='ref', location=2)
+        assert len(snv.per_lib_info) == 1
+
+    @staticmethod
+    def test_kind():
+        'It test that we can get the kind for a seqvariation'
+
+        seq_var = Snv(reference='hola', location=3,
+                      per_lib_info=[{'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':INVARIANT}]}])
+
+        assert seq_var.kind == INVARIANT
+
+        seq_var = Snv(reference='hola', location=3,
+                      per_lib_info=[{'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':DELETION}]}])
+        assert seq_var.kind == DELETION
+        seq_var = Snv(reference='hola', location=3,
+                      per_lib_info=[{'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':DELETION},
+                                               {'allele':'A', 'reads':3,
+                                               'kind':INSERTION}]},
+                                   {'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':INSERTION}]},
+                                   {'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':INVARIANT}]}])
+        assert seq_var.kind == INDEL
+
+        seq_var = Snv(reference='hola', location=3,
+                      per_lib_info=[{'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':SNP},
+                                               {'allele':'A', 'reads':3,
+                                               'kind':INSERTION}]},
+                                   {'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':DELETION}]},
+                                   {'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':INVARIANT}]}])
+        assert seq_var.kind == COMPLEX
+
+    @staticmethod
+    def test_snv_repr():
+        'It'
+        seq_var = Snv(reference='hola', location=3,
+                      annotations={'cap_enzymes':['ecoR']},
+                      per_lib_info=[{'library':'lib1',
+                                    'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':SNP},
+                                               {'allele':'A', 'reads':3,
+                                               'kind':INSERTION}]},
+                                   {'library':'library',
+                                    'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':DELETION}]},
+                                   {'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':INVARIANT}]}])
+        snv = eval(repr(seq_var))
+        assert seq_var.reference == snv.reference
+        assert seq_var.per_lib_info == snv.per_lib_info
+        assert snv.annotations['cap_enzymes'] == ['ecoR']
+
+
+    @staticmethod
+    def test_sorted_alleles():
+        'It checks that we can get the alleles sorted by the number of reads.'
+        per_lib_info = [{'alleles':[{'allele':'A', 'reads':2},
+                                   {'allele':'T', 'reads':3}]}]
+        snp = Snv(per_lib_info=per_lib_info, reference='hola', location=3)
+        alleles = snp.per_lib_info[0]['alleles']
+        assert alleles[0]['allele'] == 'T'
+        assert alleles[1]['allele'] == 'A'
+
+class SnvCaracterizationTest(unittest.TestCase):
+    'It checks that the svns are properly analyzed'
+    @staticmethod
+    def test_maf():
+        'It checks that we can calculate the maf frequency'
+        per_lib_info = [{'alleles':[{'allele':'A', 'reads':2},
+                                   {'allele':'T', 'reads':2}]}]
+        snp = Snv(per_lib_info=per_lib_info, reference='hola', location=3)
+        mafs = major_allele_frequency(snp)
+        assert mafs[0] == 0.5
+
+    @staticmethod
+    def test_variability():
+        'It checks that we can calculate the reference variability'
+        reference = SeqWithQuality(seq='Actgacttac', name='ref')
+        snp = Snv(per_lib_info=[], reference=reference, location=3)
+        ref_var = reference_variability(snp, ['snp1'])
+        assert ref_var == 10.0
+
+    @staticmethod
+    def test_calculate_pic():
+        'It checks that we are able to calculate the PIC values'
+        per_lib_info = [{'alleles':[{'allele':'A', 'reads':200},
+                                   {'allele':'T', 'reads':200}]}]
+        snp1 = Snv(per_lib_info=per_lib_info, reference='hola', location=3)
+        per_lib_info = [{'alleles':[{'allele':'A', 'reads':300},
+                                   {'allele':'T', 'reads':100}]}]
+        snp2 = Snv(per_lib_info=per_lib_info, reference='hola', location=3)
+
+        pic_1 = pic(snp1)[0]
+        pic_2 = pic(snp2)[0]
+        assert pic_1 > pic_2
+        pic_1_1 = pic(snp1)[0]
+        #we check the buffer
+        assert pic_1 == pic_1_1
+
+    @staticmethod
+    def test_remap():
+        '''It test if the remap external program works '''
+        reference = SeqWithQuality(seq='Actgacttactgtca', name='ref')
+        per_lib_info = [{'alleles':[{'allele':'C', 'reads':1, 'kind':SNP},
+                                  {'allele':'T', 'reads':1, 'kind':INVARIANT}]}]
+        snp = Snv(per_lib_info=per_lib_info, reference=reference, location=7)
+        enzymes = cap_enzymes(snp, True)
+        assert set(['HinfI', 'TscAI']) == set(enzymes)
+
+        # With a deletion
+        seq  = 'ATGATGATG' + 'gaaattc' + 'ATGATGATGTGGGAT'
+        reference = SeqWithQuality(seq=seq, name='ref')
+        per_lib_info = [{'alleles':[{'allele':'A', 'reads':1, 'kind':DELETION},
+                                  {'allele':'A', 'reads':1, 'kind':INVARIANT}]}]
+        snp = Snv(per_lib_info=per_lib_info, reference=reference, location=11)
+        enzymes = cap_enzymes(snp, True)
+        assert 'EcoRI' in enzymes
+        assert 'EcoRI' in snp.annotations['cap_enzymes']
+
+        #with an insertion
+        seq  = 'ATGATGATG' + 'gaattc' + 'ATGATGATGTGGGAT'
+        reference = SeqWithQuality(seq=seq, name='ref')
+        per_lib_info = [{'alleles':[{'allele':'A', 'reads':1, 'kind':INSERTION},
+                                  {'allele':'A', 'reads':1, 'kind':INVARIANT}]}]
+        snp = Snv(per_lib_info=per_lib_info, reference=reference, location=11)
+        enzymes = cap_enzymes(snp, True)
+        assert 'EcoRI' in enzymes
+
+        #with only one allele
+        reference = SeqWithQuality(seq='Actgacttactgtca', name='ref')
+        per_lib_info = [{'alleles':[{'allele':'C', 'reads':1, 'kind':SNP}]}]
+        snp = Snv(per_lib_info=per_lib_info, reference=reference, location=7)
+        enzymes = cap_enzymes(snp, True)
+        assert [] == enzymes
+    @staticmethod
+    def test_calculate_kind():
+        'It calculates the rerulting kind'
+        assert calculate_kind(SNP, INVARIANT) == SNP
+        assert calculate_kind(INSERTION, DELETION) == INDEL
+        assert calculate_kind(COMPLEX, SNP) == COMPLEX
+        assert calculate_kind(SNP, INDEL) == COMPLEX
+
+class SnvIOTest(unittest.TestCase):
+    ''' It checks if we have problems with remaps and it functions'''
+
+    @staticmethod
+    def test_svns_in_file():
+        'It we can read the svn file'
+        seq_var = Snv(reference='hola', location=3,
+                      per_lib_info=[{'library':'library2',
+                                     'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':SNP},
+                                               {'allele':'A', 'reads':3,
+                                               'kind':INSERTION}]},
+                                   {'library':'library',
+                                    'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':DELETION}]},
+                                   {'library':'library3',
+                                    'alleles':[{'allele':'A', 'reads':3,
+                                               'kind':INVARIANT}]}])
+        fhand = StringIO(repr(seq_var))
+        snv   = snvs_in_file(fhand).next()
+        assert seq_var.reference == snv.reference
+        assert seq_var.per_lib_info == snv.per_lib_info
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_SeqVariation_init']
+    unittest.main()
---
+++ biolib/src/test/snv/snp_cleaner_test.py
@@ -0,0 +1,323 @@
+'''
+Created on 2009 uzt 30
+
+@author: peio
+'''
+# Copyright 2009 Jose Blanca, Peio Ziarsolo, COMAV-Univ. Politecnica Valencia
+# This file is part of biolib.
+# biolib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# biolib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with biolib. If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from biolib.seqvar.seqvariation import (SNP, DELETION, INVARIANT, Snv, COMPLEX)
+from biolib.seqvar.snp_cleaner import (#create_major_allele_freq_filter,
+                                       create_close_to_seqvar_filter,
+                                       create_cap_enzyme_filter,
+                                       create_high_variable_region_filter,
+                                       create_is_variable_in_some_filter,
+                                       create_is_variable_in_aggregate_filter,
+                                       create_bad_quality_reads_cleaner,
+                                       create_snv_close_to_limit_filter,
+                                       create_major_allele_freq_filter,
+                                       create_read_number_cleaner,
+                                       create_alleles_n_cleaner,
+                                       create_kind_filter,
+                                       create_reference_list_filter)
+from biolib.seqs import SeqWithQuality
+
+class SeqVariationFilteringTest(unittest.TestCase):
+    'It checks the filtering methods.'
+
+    @staticmethod
+    def test_enzyme_filter():
+        'It test the enzyme filter'
+        seq  = 'ATGATGATG' + 'gaaattc' + 'ATGATGATGTGGGAT'
+        reference = SeqWithQuality(seq=seq, name='ref')
+        snp = Snv(per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':4, 'kind':INVARIANT},
+                                  {'allele':'A', 'reads':2, 'kind':DELETION}]}],
+                        location=11, reference=reference)
+        cap_enzime = create_cap_enzyme_filter(True)
+        snp = (snp, 'context')
+        assert cap_enzime(snp) == True
+
+    @staticmethod
+    def test_major_allele_freq_filter_snv():
+        'It test the first allele percent filter'
+        snp = Snv(per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':4, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'alleles':[{'allele':'A', 'reads':2, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}],
+                  location=11, reference='reference')
+        snp = (snp, 'context')
+        frec_cleaner = create_major_allele_freq_filter(0.51)
+        assert frec_cleaner(snp)
+
+        snp = Snv(location=11, reference='reference', per_lib_info=[
+                        {'library':'somelib',
+                         'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'library':'somelib2',
+                         'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}])
+        snp = (snp, 'context')
+        frec_cleaner = create_major_allele_freq_filter(0.51)
+        assert not frec_cleaner(snp)
+
+    @staticmethod
+    def test_high_variable_region_filter():
+        'It test percent_variations_in_seq_ref filter'
+        reference = SeqWithQuality(seq='atatat')
+        snp = Snv(location=1, reference=reference, per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}])
+
+        snp1 = Snv(location=4, reference=reference, per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}])
+
+        snp2 = Snv(location=7, reference=reference, per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}])
+
+        context = [snp, snp1, snp2]
+        snp     = (snp1, context)
+        # This reference variability is 50
+        filter40 = create_high_variable_region_filter(40)
+        assert not filter40(snp)
+
+        filter60 = create_high_variable_region_filter(60)
+        assert filter60(snp)
+
+    @staticmethod
+    def test_create_filter_close_seqvar():
+        'It test the first allele percent filter'
+        reference = 'reference'
+        snp = Snv(location=10, reference=reference, per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}])
+
+        snp1 = Snv(location=15, reference=reference, per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}])
+
+        snp2 = Snv(location=20, reference=reference, per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}])
+        context = [snp, snp1, snp2]
+        snp = (snp, context)
+
+        filter4 = create_close_to_seqvar_filter(5)
+        filter7 = create_close_to_seqvar_filter(7)
+        assert filter4(snp)
+        assert not filter7(snp)
+
+    @staticmethod
+    def test_allele_number_filter():
+        'It test percent_variations_in_seq_ref filter'
+        reference = 'atatat'
+        snp = Snv(location=10, reference=reference, per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}])
+
+
+
+        snp1 = Snv(location=15, reference=reference, per_lib_info=[
+                    {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT}]}])
+
+        snp2 = Snv(location=20, reference=reference, per_lib_info=[
+                      {'library':'lib1',
+                       'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                  {'allele':'T', 'reads':3, 'kind':SNP}]},
+                      {'library':'lib2',
+                       'alleles':[{'allele':'T', 'reads':2,'kind':SNP}]}])
+
+        allele_number_filter = create_is_variable_in_some_filter()
+        assert  allele_number_filter((snp, [snp]))
+        assert not allele_number_filter((snp1, [snp]))
+        assert allele_number_filter((snp2, [snp]))
+
+        allele_number_filter = create_is_variable_in_some_filter(
+                                                           libraries=['lib2'])
+        assert not allele_number_filter((snp2, [snp]))
+
+    @staticmethod
+    def test_is_variable_in_aggregate_filter():
+        'It test percent_variations_in_seq_ref filter'
+        reference = 'atatat'
+        snv = Snv(location=10, reference=reference, per_lib_info=[
+                    {'library':'lib1',
+                     'alleles':[{'allele':'A', 'reads':3, 'kind':SNP,
+                                 'qualities':[]}]},
+                    {'library':'lib2',
+                     'alleles':[{'allele':'T', 'reads':2, 'kind':SNP,
+                                 'qualities':[]}]},
+                    {'library':'lib3',
+                     'alleles':[{'allele':'T', 'reads':2, 'kind':SNP,
+                                 'qualities':[]}]}])
+        snv = snv, [snv]
+        filter_ = create_is_variable_in_aggregate_filter()
+        assert filter_(snv)
+
+        filter_ = create_is_variable_in_aggregate_filter(libraries=['lib3',
+                                                                    'lib2'])
+        assert not filter_(snv)
+
+
+    @staticmethod
+    def test_create_seqvar_close_to_limt():
+        'It tests the close to limit filter function '
+        snp = Snv(location=4, reference='atatatatat', per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]},
+                        {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT},
+                                    {'allele':'T', 'reads':2,'kind':SNP}]}])
+        snp = (snp, 'context')
+        filter_ = create_snv_close_to_limit_filter(3)
+        assert filter_(snp)
+        filter_ = create_snv_close_to_limit_filter(5)
+        assert not filter_(snp)
+
+    @staticmethod
+    def test_remove_bad_quality_reads():
+        'It test it we can remove the alleles with bad qualities'
+        #the good reads in 454 are around a quality of 25 to 40
+        #a bad letter is less than 20
+        #for sanger the phred quality values are related to the probability
+        #of having an error in the sequence
+        #http://www.phrap.com/phred/
+        #Phred quality score  Probability base is wrong   Accuracy of the base
+        #       10                   1 in 10                  90%
+        #       20                   1 in 100                 99%
+        #       30                   1 in 1,000               99.9%
+        #       40                   1 in 10,000              99.99%
+        #       50                   1 in 100,000             99.999%
+        bad_quality_cleaner = create_bad_quality_reads_cleaner(25)
+        snp = Snv(location=4, reference='atatatatat', per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':4, 'kind':INVARIANT,
+                                     'qualities':[30, 30, 30, 20]},
+                                    {'allele':'T', 'reads':2,'kind':SNP,
+                                     'qualities':[30, 20]}]}])
+        snp = (snp, 'context')
+        snp = bad_quality_cleaner(snp)[0]
+        assert len(snp.per_lib_info[0]['alleles']) == 2
+        assert snp.per_lib_info[0]['alleles'][0]['reads'] == 3
+
+        snp = Snv(location=4, reference='atatatatat', per_lib_info=[
+                {'alleles':[{'allele':'A', 'reads':3, 'kind':INVARIANT,
+                             'qualities':[30, 30, 30, 20]},
+                            {'allele':'T', 'reads':2,'kind':SNP,
+                             'qualities':[20, 20]}]}])
+        snp = (snp, 'context')
+        snp = bad_quality_cleaner(snp)[0]
+
+        assert len(snp.per_lib_info[0]['alleles']) == 1
+        assert snp.per_lib_info[0]['alleles'][0]['reads'] == 3
+
+        snp = Snv(location=4, reference='atatatatat', per_lib_info=[
+                {'alleles':[{'allele':'AT', 'reads':4, 'kind':DELETION,
+                             'qualities':[None, None, None, None]},
+                            {'allele':'T', 'reads':2,'kind':SNP,
+                             'qualities':[30, 30]}]}])
+        snp = (snp, 'context')
+        snp = bad_quality_cleaner(snp)[0]
+
+        assert len(snp.per_lib_info[0]['alleles']) == 2
+        assert snp.per_lib_info[0]['alleles'][0]['reads'] == 4
+        assert len(snp.per_lib_info[0]['alleles'][0]['qualities']) == 4
+
+        snp = Snv(location=4, reference='atatatatat', per_lib_info=[
+                {'alleles':[{'allele':'AT', 'reads':4, 'kind':DELETION},
+                            {'allele':'T', 'reads':2,'kind':SNP }]}])
+        snp = (snp, 'context')
+        snp = bad_quality_cleaner(snp)[0]
+        assert len(snp.per_lib_info[0]['alleles']) == 2
+
+
+
+    @staticmethod
+    def test_read_number_cleaner():
+        'Tests read number_cleaner'
+        snp = Snv(location=4, reference='atatatatat', per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':4, 'kind':INVARIANT,
+                                     'quality':[30, 30, 30, 20]},
+                                    {'allele':'T', 'reads':1,'kind':SNP,
+                                     'quality':[30, 20]}]}])
+        snp = (snp, 'context')
+        read_number_cleaner = create_read_number_cleaner(2)
+        snp = read_number_cleaner(snp)[0]
+        assert len(snp.per_lib_info[0]['alleles']) == 1
+
+        snp = Snv(location=4, reference='atatatatat', per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':1, 'kind':INVARIANT,
+                                     'quality':[30, 30, 30, 20]},
+                                    {'allele':'T', 'reads':1,'kind':SNP,
+                                     'quality':[30, 20]}]}])
+        snp = (snp, 'context')
+        read_number_cleaner = create_read_number_cleaner(2)
+        snp = read_number_cleaner(snp)
+        assert snp is None
+
+    @staticmethod
+    def test_alleles_n_cleaner():
+        'It checks the removal of alleles N or NN'
+        snp = Snv(location=4, reference='atatatatat', per_lib_info=[
+                        {'alleles':[{'allele':'N', 'reads':10},
+                                    {'allele':'nN', 'reads':10},
+                                    {'allele':'T', 'reads':10}]}])
+        snp = (snp, 'context')
+        n_cleaner = create_alleles_n_cleaner()
+        snp = n_cleaner(snp)[0]
+        assert len(snp.per_lib_info[0]['alleles']) == 1
+
+    @staticmethod
+    def test_kind_filter():
+        'It checks the filter by snv kind'
+        snv = Snv(location=4, reference='atatatatat', per_lib_info=[
+                        {'alleles':[{'allele':'A', 'reads':10, 'kind':SNP},
+                                    {'allele':'T', 'reads':10,
+                                     'kind':INVARIANT}]}])
+        snv = (snv, 'context')
+        kind_filter = create_kind_filter([SNP, COMPLEX])
+        assert kind_filter(snv)
+
+    @staticmethod
+    def test_reference_in_ref_list():
+        'It can filter out the snvs with a reference not in the given list'
+        allowed_refs = ['ref1']
+        snv1 = Snv(location=4, reference='ref1', per_lib_info=[])
+        snv2 = Snv(location=4, reference='ref2', per_lib_info=[])
+        filter_ = create_reference_list_filter(allowed_refs)
+
+        assert filter_((snv1, [snv1]))
+        assert not filter_((snv2, [snv2]))
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_SeqVariation_init']
+    unittest.main()
