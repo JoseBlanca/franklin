a7cf676dbbacf2eeaef6dcfe1578bb41fc8c8cb1
Jose Blanca <jblanca@btc.upv.es>
2009-12-21 11:36:53 +0000

    improved setup

--- biolib/src/setup.py
+++ biolib/src/setup.py
@@ -3,6 +3,58 @@ Created on 25/03/2009
 
 @author: jose blanca
 '''
+#taken from django-tagging
+
+import os
+
+PACKAGE_DIR = 'biolib'
+SCRIPTS_DIR = 'scripts'
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files, modules = [], [], []
+root_dir = os.path.dirname(__file__)
+pieces = fullsplit(root_dir)
+if pieces[-1] == '':
+    len_root_dir = len(pieces) - 1
+else:
+    len_root_dir = len(pieces)
+
+for dirpath, dirnames, filenames in os.walk(os.path.join(root_dir,
+                                                         PACKAGE_DIR)):
+    if '__init__.py' in filenames:
+        package = '.'.join(fullsplit(dirpath)[len_root_dir:])
+        packages.append(package)
+        for filename in os.listdir(dirpath):
+            if (filename.startswith('.') or filename.startswith('_') or
+                not filename.endswith('.py')):
+                continue
+            modules.append(package + '.' + filename)
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+scripts = []
+for dirpath, dirnames, filenames in os.walk(os.path.join(root_dir,
+                                                         SCRIPTS_DIR)):
+    for filename in filenames:
+        if filename == '__init__.py':
+            continue
+        elif filename.endswith('.py'):
+            scripts.append(os.path.join(dirpath, filename))
 
 from setuptools import setup
 setup(
@@ -13,11 +65,10 @@ setup(
     author_email='jblanca@btc.upv.es',
     description='Some genomics related classes',
     # package structure
-    packages=['biolib', 'biolib.snv', 'biolib.db', 'biolib.gmod', 'biolib.seq',
-              'biolib.utils', 'biolib.pipelines'],
+    packages=packages,
     package_dir={'':'.'},
-    package_data={'mypkg': ['data/*']},
+    #py_modules = modules,
+    package_data={'biolib': ['data/*']},
     requires=['BioPython', 'sqlalchemy', 'matplotlib'],
-    scripts=['scripts/run_cleanning_pipeline.py', 'scripts/snv/run_bwa.py',
-             'scripts/seqio.py', ]
+    #scripts=scripts,
 )
