014afa6741af5fc03055a1a781ef313c79cc1514
Jose Blanca <jblanca@btc.upv.es>
2010-05-24 14:01:37 +0200

    config function moved to its own module

--- franklin/backbone/create_project.py
+++ franklin/backbone/create_project.py
@@ -22,11 +22,16 @@ Created on 29/01/2010
 # You should have received a copy of the GNU Affero General Public License
 # along with franklin. If not, see <http://www.gnu.org/licenses/>.
 
-import os, tempfile, shutil
+import os
 
 from configobj import ConfigObj
 
 from franklin.backbone.specifications import BACKBONE_DIRECTORIES
+from franklin.utils.misc_utils import OrderedDict
+from franklin.utils.config_utils import (pretyfy_config, add_default_values,
+                                         validate_config, STRING, INTEGER,
+                                         BOOLEAN, INTEGER_OR_BOOL, STRING_LIST,
+                                         NUMBER_LIST)
 
 def create_project(name, directory=None, configuration=None):
     'It creates the files that define a project'
@@ -61,40 +66,16 @@ def create_project(name, directory=None, configuration=None):
 
     pretyfy_config(config.filename)
 
-    print open(config.filename).read()
-
     return settings_path
-
-def pretyfy_config(fpath):
-    'It adds tabs to the config'
-    new_config = tempfile.NamedTemporaryFile()
-
-    section_depth = 0
-    for line in open(fpath):
-        line = line.strip()
-        if line.startswith('['):
-            section_depth = line.count('[')
-
-        if line.startswith('['):
-            line = '    ' * (section_depth  - 1) + line
-        else:
-            line = '    ' * (section_depth - 0) + line
-
-        line = line + '\n'
-        if line.startswith('[') and line.count('[') == 1:
-            new_config.write('\n')
-        new_config.write(line)
-
-    shutil.move(new_config.name, fpath)
 
 def create_configuration(config_path):
     'It returns a validated configObj'
 
     config = ConfigObj(config_path, unrepr=True)
 
-    _add_default_values(config, DEFAULT_CONFIGURATION)
+    add_default_values(config, DEFAULT_CONFIGURATION)
 
-    _validate_config(config, DEFAULT_CONFIGURATION)
+    validate_config(config, DEFAULT_CONFIGURATION)
 
     return config
 
@@ -103,125 +84,13 @@ def _make_default_config(project_path, name, config_data):
 
     config = ConfigObj(unrepr=True)
 
-    _add_default_values(config, DEFAULT_CONFIGURATION)
+    add_default_values(config, DEFAULT_CONFIGURATION)
 
     _add_some_settings(config, project_path, name, config_data)
 
-    _validate_config(config, DEFAULT_CONFIGURATION)
+    validate_config(config, DEFAULT_CONFIGURATION)
 
     return config
-
-STRING = (basestring,)
-INTEGER = (int,)
-BOOLEAN = (bool,)
-FLOAT = (float,)
-NUMBER = (float, int)
-INTEGER_OR_BOOL = (int, bool)
-
-def _list(value, parameter, type_):
-    'It checks that the values in the list are the given type_'
-    if not isinstance(value, list) and not isinstance(value, tuple):
-        msg = 'In parameter %s, value %s is not a list' % (parameter,
-                                                            str(value))
-        raise ValueError(msg)
-    for item in value:
-        _check_type(item, parameter, type_)
-
-STRING_LIST = (_list, STRING)
-FLOAT_LIST = (_list, FLOAT)
-NUMBER_LIST = (_list, NUMBER)
-
-def _check_type(value, parameter, types):
-    'It check that the value is an string or None'
-    if value is None:
-        return
-    if not any([isinstance(value, type_) for type_ in types]):
-        msg = 'In parameter %s, value %s is not a %s' % (parameter,
-                                                         str(value),
-                                                         str(type_))
-        raise ValueError(msg)
-
-def _validate_config(config, validation_spec):
-    'It raises an error if some value is not ok'
-    errors = _validate_config_recursive(config, validation_spec)
-    if errors:
-        msg = 'Erros found in the configuration for the following '
-        msg += 'sections:values:type -> '
-        errors = ', '.join(['%s:%s:%s' % (error) for error in errors])
-        msg += errors
-        raise ValueError(msg)
-
-def _validate_config_recursive(config, validation_spec, errors=None):
-    'It returns the errors found in the configuration'
-    validator_strs = {(basestring,): 'string',
-                      (int,): 'integer',
-                      (bool,): 'bolean',
-                      (float,): 'float',
-                      (float, int): 'number',
-                      (int, bool): 'integer or boolean',
-                      (_list, STRING): 'list of strings',
-                      (_list, FLOAT): 'list of floats',
-                      (_list, NUMBER): 'list of numbers'}
-
-    if errors is None:
-        errors = []
-    for key, value in config.items():
-        #if key == '__many__':
-            #this is only used for the validation, not for the default
-            #continue
-
-        #are we dealing with a dict or with just a value?
-        if 'items' in dir(value):
-            if '__many__' in validation_spec:
-                spec_value = validation_spec['__many__']
-            elif key not in validation_spec:
-                #some sections do not require validation
-                spec_value = None
-            else:
-                spec_value = validation_spec[key]
-            if spec_value is not None:
-                _validate_config_recursive(value, spec_value, errors)
-        else:
-            #are there a default value?
-            if key == 'kind':
-                pass
-            spec_value = validation_spec[key]
-            validator = validation_spec[key][0]
-            validator_str = validator_strs[validator]
-            try:
-                if validator in (STRING, INTEGER, BOOLEAN, FLOAT,
-                                 INTEGER_OR_BOOL):
-                    _check_type(value, key, validator)
-                elif validator in (STRING_LIST, FLOAT_LIST, NUMBER_LIST):
-                    validator[0](value, key, validator[1])
-                else:
-                    raise RuntimeError('Unknown validator ' + str(validator))
-
-            except ValueError:
-                errors.append((key, value, validator_str))
-    return errors
-
-def _add_default_values(config, defaults):
-    'It adds the default values to the configuration'
-
-    for key, value in defaults.items():
-        if key == '__many__':
-            #this is only used for the validation, not for the default
-            continue
-
-        #are we dealing with a dict or with just a value?
-        if 'items' in dir(value):
-            if key not in config:
-                config[key] = {}
-            _add_default_values(config[key], value)
-        else:
-            #are there a default value?
-            if key in config:
-                continue
-            if len(value) > 1:
-                default_value = value[1]
-                config[key] = default_value
-
 def _add_some_settings(config, project_path, name, config_data):
     'It adds some extra default parameters'
 
@@ -308,8 +177,6 @@ def _add_some_settings(config, project_path, name, config_data):
     config['snv_filters']['filter12']['name'] = 'ref_not_in_list'
     config['snv_filters']['filter12']['use'] = False
     config['snv_filters']['filter12']['list_path'] = 'path_to_file_with_list'
-
-from franklin.utils.misc_utils import OrderedDict
 
 DEFAULT_CONFIGURATION = OrderedDict([
            ('General_settings',
