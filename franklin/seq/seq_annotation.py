c49c1ac3f2e5730940a920dc0cda5c7be0befefc
Peio <pziarsolo@upvnet.upv.es>
2010-10-06 09:33:05 +0200

    added protein change by snv annotator

--- franklin/seq/seq_annotation.py
+++ franklin/seq/seq_annotation.py
@@ -26,16 +26,22 @@ Created on 15/01/2010
 from xml.parsers.expat import ExpatError
 from Bio import SeqIO
 from Bio.SeqFeature import  FeatureLocation
-from Bio.Alphabet import generic_dna, generic_protein
+from Bio.Alphabet import generic_dna, generic_protein, IUPAC
 
 from franklin.alignment_search_result import (BlastParser,
-                                            FilteredAlignmentResults)
-from franklin.utils.cmd_utils import  create_runner, b2gpipe_runner
+                                              FilteredAlignmentResults)
+from franklin.snv.snv_annotation import INVARIANT, SNP, create_alleles, DELETION,\
+    INSERTION, SNV_TYPES
+from franklin.utils.cmd_utils import  create_runner
 from franklin.seq.seqs import SeqFeature, get_seq_name, Seq
 from franklin.utils.seqio_utils import get_content_from_fasta
 from franklin.seq.seq_analysis import infer_introns_for_cdna, get_orthologs
 from franklin.seq.readers import guess_seq_file_format
+from franklin.seq.writers import temp_fasta_file
 from franklin.utils.misc_utils import get_fhand
+from franklin.alignment_search_result import build_relations_from_aligment
+from franklin.coordsystem import CoordSystem
+from tempfile import NamedTemporaryFile
 
 def create_ortholog_annotator(blast, reverse_blast, species):
     '''It creates a function factory that calculates all the orthologs between
@@ -118,6 +124,283 @@ def _get_descriptions_from_blasts(blasts):
             msg = str(error) + ':%s' % blast_fhand.name
             raise ExpatError(msg)
     return seq_annot
+
+#def create_polia_annotator():
+#    'It creates a function that annotates polia'
+#    parameters = {'min_score':'10', 'end':'x', 'incremental_dist':'20',
+#                  'fixed_dist':None}
+#    runner = create_runner(tool='trimpoly', parameters=parameters)
+#
+#    def annotate_polia(sequence):
+#        'It annotates the polia'
+#        if sequence is None:
+#            return
+#        trimpoly_out_fhand = runner(sequence)['sequence']
+#        trimp_data = trimpoly_out_fhand.readline().split()
+#        end5 = int(trimp_data[2]) - 1
+#        end3 = int(trimp_data[3])
+#
+#        if end3 != 0 or end5 != len(sequence):
+#            polia = SeqFeature(location=FeatureLocation(end5, end3),
+#                             type='polia', qualifiers={})
+#            sequence.features.append(polia)
+#        return sequence
+#
+#    return annotate_polia
+
+def create_prot_change_annotator():
+    '''It creates a function that caracterizes if the annotated snv produces a
+    change in the protein'''
+    def prot_change_annotator(sequence):
+        'It annotates the change in the protein by the snv'
+        if sequence is None:
+            return
+
+        for orf in sequence.get_features(kind='orf'):
+            for snv in sequence.get_features(kind='snv'):
+                protein_change = protein_change_annotator(sequence, orf, snv)
+                snv.qualifiers['protein_change'] = protein_change
+
+    return prot_change_annotator
+
+def  protein_change_annotator(sequence, orf, snv):
+    '''It annotates the protein changes and stores it in the snv qualifiers
+    structure'''
+    codons, location, snv_pos_in_orf = _get_codons(sequence, orf, snv)
+
+    protein_changes = {}
+    protein_changes['alleles'] = {}
+    for allele in snv.qualifiers['alleles']:
+        protein_changes['alleles'][allele] = {}
+        allele_kind = allele[1]
+        aa = _translate(codons[allele])
+        #print aa, codons[allele]
+        protein_changes['alleles'][allele]['aa'] = aa
+        if aa == '*':
+            allele_type = 'stop'
+        elif allele_kind in (SNP, INVARIANT):
+            allele_type = 'unknown'
+        elif allele_kind in  (INSERTION, DELETION):
+            insert_len = len(allele[0])
+            codon_pos = insert_len % 3
+            if codon_pos == 0:
+                allele_type = SNV_TYPES[allele_kind]
+            else:
+                allele_type = 'frame_change'
+        protein_changes['alleles'][allele]['kind'] = allele_type
+
+    change_kind = _get_prot_change_kind(protein_changes)
+    location = _get_change_locations(snv_pos_in_orf, location, change_kind)
+
+    protein_changes['kind'] = change_kind
+    protein_changes['location'] = location
+    return protein_changes
+
+def _get_prot_change_kind(protein_change):
+    'it merges the protein change kind in a general'
+    kinds = [allele['kind'] for allele in protein_change['alleles'].values()]
+    if 'insertion' in kinds or 'deletion' in kinds:
+        kind = 'indel'
+    elif 'stop' in kinds or 'frame_change' in kinds:
+        kind = 'breakage'
+    else:
+        aas = [allele['aa'] for allele in protein_change['alleles'].values()]
+        if len(set(aas)) == 1:
+            kind = 'synonym'
+        else:
+            kind = 'substitution'
+    return kind
+
+def _get_change_locations(snv_pos_in_orf, location, kind):
+    'It locates the protein change in the sequence'
+    if kind  == 'indel':
+        location = 'utr'
+    elif location == 'orf':
+        location = 'codon_%d' % ((snv_pos_in_orf + 1) % 3)
+    elif location is None:
+        location = 'unknown'
+    return location
+
+def _locate_codons_in_orf(sequence, orf, snv):
+    'It locates the snv in the orf coordinate system'
+    query_name = sequence.name
+    orf_seq = orf.qualifiers['dna']
+    subject_name = 'subject'
+    subject_fhand = NamedTemporaryFile(suffix='.fasta')
+    subject_fhand.write('>%s\n%s\n' % (subject_name, orf_seq))
+    subject_fhand.flush()
+    parameters   = {'subject':subject_fhand.name}
+    aligner      = create_runner(tool='water', parameters=parameters)
+    result_fhand = aligner(sequence)['water']
+    relations = build_relations_from_aligment(result_fhand,
+                                              query_name=sequence.name,
+                                              subject_name=subject_name)
+    #print relations
+    coord = CoordSystem(relations=[relations])
+
+    # snv .positions
+    snv_pos = snv.location.start.position
+
+
+    try:
+        snv_in_orf = coord.transform(from_mol=query_name, to_mol=subject_name,
+                                     position=snv_pos)
+    except RuntimeError:
+        snv_in_orf = None
+
+    #print snv_in_orf_start, snv_in_orf_end
+
+    orf_start = 0
+    orf_end   = len(orf.qualifiers['dna']) - 1
+    orf_start_limit_in_seq = coord.transform(from_mol=subject_name,
+                                             to_mol=query_name,
+                                             position=orf_start)
+    orf_end_limit_in_seq   = coord.transform(from_mol=subject_name,
+                                             to_mol=query_name,
+                                             position=orf_end)
+
+    if snv_in_orf is None:
+        # it can be utr3, utr5, or None
+        if snv_pos < orf_start_limit_in_seq:
+            position = 'utr5'
+        elif snv_pos > orf_end_limit_in_seq:
+            position = 'in utr3'
+        else:
+            position = None
+        codon_start = None
+        snv_in_orf  = None
+    else:
+        start_codon_pos = snv_in_orf % 3
+        codon_start     = snv_in_orf - start_codon_pos
+        position        = 'orf'
+
+    return (position, codon_start, snv_in_orf)
+
+def _translate(seq):
+    'it translates a sequence into a protein'
+    rna = Seq(seq, IUPAC.ambiguous_rna)
+    return str(rna.translate())
+
+def _get_codons_with_alleles(codon_pos, snv_pos, alleles, sequence):
+    'It get codons sequence giving section'
+    codons = {}
+    for allele, kind in alleles:
+        #print codon_pos, snv_pos
+        if kind in (SNP, INVARIANT):
+            left  = sequence[codon_pos: snv_pos]
+            rigth = sequence[snv_pos + 1: codon_pos + 3]
+            codon = left + allele + rigth
+
+        if kind == INSERTION:
+            left  = sequence[codon_pos: snv_pos]
+            rigth = sequence[snv_pos: codon_pos + 3]
+            codon = left + allele + rigth
+
+            index = snv_pos + 1
+            while len(codon) % 3 != 0:
+                codon += sequence[index]
+                index += 1
+
+        elif kind == DELETION :
+            left  = sequence[codon_pos: snv_pos]
+            del_allele = sequence[snv_pos + len(allele):snv_pos + len(allele) + len(allele)]
+            codon = left + del_allele
+
+            index = snv_pos + 1
+            while len(codon) % 3 != 0:
+                codon += sequence[index]
+                index += 1
+
+        codons[(allele, kind)] = codon
+
+    return codons
+
+def _get_codons(sequence, orf, snv):
+    'It gets the codons afected by the snv'
+    location, codon_pos, snv_pos = _locate_codons_in_orf(sequence, orf, snv)
+    orf_seq = orf.qualifiers['dna']
+    if codon_pos is not None and snv_pos is not None:
+        alleles = snv.qualifiers['alleles'].keys()
+        codons  = _get_codons_with_alleles(codon_pos, snv_pos, alleles, orf_seq)
+    else:
+        codons =  None
+    return codons, location, snv_pos
+
+
+#def _locate_codons_and_snvs_in_orf(sequence, orf, snv):
+#    'It locates the snv in the orf coordinate system'
+#    query_name = sequence.name
+#    orf_seq = orf.qualifiers['dna']
+#    subject_name = 'subject'
+#    subject_fhand = NamedTemporaryFile(suffix='.fasta')
+#    subject_fhand.write('>%s\n%s\n' % (subject_name, orf_seq))
+#    subject_fhand.flush()
+#    parameters   = {'subject':subject_fhand.name}
+#    aligner      = create_runner(tool='water', parameters=parameters)
+#    result_fhand = aligner(sequence)['water']
+#    relations = build_relations_from_aligment(result_fhand,
+#                                              query_name=sequence.name,
+#                                              subject_name=subject_name)
+#    #print relations
+#    coord = CoordSystem(relations=[relations])
+#
+#    # snv .positions
+#    snv_start_pos = snv.location.start.position
+#    snv_end_pos   = snv.location.end.position
+#
+#    try:
+#        snv_in_orf_start = coord.transform(from_mol=query_name,
+#                                           to_mol=subject_name,
+#                                           position=snv_start_pos)
+#    except RuntimeError:
+#        snv_in_orf_start = None
+#    try:
+#        snv_in_orf_end = coord.transform(from_mol=query_name,
+#                                         to_mol=subject_name,
+#                                         position=snv_end_pos)
+#    except RuntimeError:
+#        snv_in_orf_end = None
+#
+#    #print snv_in_orf_start, snv_in_orf_end
+#
+#    orf_start = 0
+#    orf_end   = len(orf.qualifiers['dna']) - 1
+#    orf_start_limit_in_seq = coord.transform(from_mol=subject_name,
+#                                             to_mol=query_name,
+#                                             position=orf_start)
+#    orf_end_limit_in_seq   = coord.transform(from_mol=subject_name,
+#                                             to_mol=query_name,
+#                                             position=orf_end)
+#
+#    if snv_in_orf_start is None and snv_in_orf_end is None:
+#        # if we don't now any position, we can not know the change between
+#        # codons
+#        pass
+#    elif snv_in_orf_start is None:
+#        # if start in utr5, use orf start as start of protein coding
+#        if  snv_start_pos < orf_start_limit_in_seq:
+#            snv_in_orf_start = orf_start
+#        else:
+#            snv_in_orf_start = None
+#    elif snv_in_orf_end is None:
+#        # if end in utr3, use end of orf as end of pretein coding
+#        if snv_end_pos > orf_end_limit_in_seq:
+#            snv_in_orf_end = orf_end
+#        else:
+#            snv_in_orf_end = None
+#
+#    if snv_in_orf_start is not None and snv_in_orf_end is not None:
+#        start_codon_pos = snv_in_orf_start % 3
+#        end_codon_pos   = snv_in_orf_end  % 3
+#
+#        codon_start = snv_in_orf_start - start_codon_pos
+#        codon_end   = snv_in_orf_end - end_codon_pos + 3
+#
+#        return (codon_start, codon_end), (snv_in_orf_start, snv_in_orf_end)
+#    return None, None
+
+
+
 
 def create_microsatellite_annotator():
     'It creates a function that'
