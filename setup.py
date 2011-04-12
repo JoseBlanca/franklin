aed4cc22e3e2cb77d2d2fbe94dd1bef71ccb5562
Jose Blanca <jblanca@upv.es>
2011-04-12 15:27:10 +0200

    setup is now capable of installing linux binaries

--- setup.py
+++ setup.py
@@ -5,21 +5,44 @@ Created on 25/03/2009
 '''
 #taken from django-tagging
 
-import os, sys, imp
-#from setuptools import setup
+import imp
+import os, sys, glob, fnmatch
 from distutils.core import setup
+
 import franklin
 
-PACKAGE_DIR = 'franklin'
-SCRIPTS_DIR = 'scripts'
 
-#dependencies
-MODULES = {'Bio':        {'required': True},
-           'configobj':  {'required': True},
-           'psubprocess':{'msg':'no parallel processing will be possible'},
-           'pysam':      {'msg':'SNP calling will fail'},
-           'matplotlib': {'msg':'some statistics will fail'}
-           }
+def opj(*args):
+    path = os.path.join(*args)
+    return os.path.normpath(path)
+
+def find_data_files(srcdir, *wildcards, **kw):
+    # get a list of all files under the srcdir matching wildcards,
+    # returned in a format to be used for install_data
+    def walk_helper(arg, dirname, files):
+        if '.svn' in dirname or '.git' in dirname:
+            return
+        names = []
+        lst, wildcards = arg
+        for wc in wildcards:
+            wc_name = opj(dirname, wc)
+            for f in files:
+                filename = opj(dirname, f)
+                if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(filename):
+                    names.append(filename)
+        if names:
+            lst.append( (dirname, names ) )
+
+    file_list = []
+    recursive = kw.get('recursive', True)
+    if recursive:
+        os.path.walk(srcdir, walk_helper, (file_list, wildcards))
+    else:
+        walk_helper((file_list, wildcards),
+                    srcdir,
+                    [os.path.basename(f) for f in glob.glob(opj(srcdir, '*'))])
+    return file_list
+
 def check_modules(modules):
     'It check the modules needed'
     all_optionals = True
@@ -45,42 +68,6 @@ def check_modules(modules):
             print 'Installation aborted'
             sys.exit()
 
-#
-#try:
-#    import Bio
-#except:
-#    print 'BioPython is required and is not installed for %s' % python_exec
-#    print 'Installation aborted'
-#    sys.exit(1)
-#try:
-#    import configobj
-#except:
-#    print'ConfigObj is required and is not installed for %s' % python_exec
-#    print 'Installation aborted'
-#    sys.exit(1)
-#all_optionals = True
-#try:
-#    import psubprocess
-#except:
-#    print 'psubprocess is not installed for %s, no parallel processing will be possible' % python_exec
-#    all_optionals = False
-#try:
-#    import pysam
-#except:
-#    print 'pysam is not installed for %s, SNP calling will fail' % python_exec
-#    all_optionals = False
-#try:
-#    imp.find_module('matplotlib')
-#    #import matplotlib
-#except:
-#    print 'matplotlib is not installed for %s, some statistics will fail' % python_exec
-#    all_optionals = False
-#if not all_optionals:
-#    answer = raw_input('Some optional requirements were not met, do you want to continue the installation (y/n)? ')
-#    if answer.lower() not in ('y', 'yes'):
-#        print 'Installation aborted'
-#        sys.exit()
-
 def fullsplit(path, result=None):
     """
     Split a pathname into components (the opposite of os.path.join) in a
@@ -95,59 +82,92 @@ def fullsplit(path, result=None):
         return result
     return fullsplit(head, [tail] + result)
 
+def _guess_packages(directory):
+    # Compile the list of packages available, because distutils doesn't have
+    # an easy way to do this.
+    packages, data_files, modules = [], [], []
+
+    pieces = fullsplit(directory)
+    if pieces[-1] == '':
+        len_root_dir = len(pieces) - 1
+    else:
+        len_root_dir = len(pieces)
+
+    for dirpath, dirnames, filenames in os.walk(os.path.join(directory,
+                                                             PACKAGE_DIR)):
+        if '__init__.py' in filenames:
+            package = '.'.join(fullsplit(dirpath)[len_root_dir:])
+            packages.append(package)
+            for filename in os.listdir(dirpath):
+                if (filename.startswith('.') or filename.startswith('_') or
+                    not filename.endswith('.py')):
+                    continue
+                modules.append(package + '.' + filename)
+        elif filenames:
+            data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+    return packages
+
+def _guess_scripts(directory, wanted_scripts):
+    scripts = []
+    for dirpath, dirnames, filenames in os.walk(os.path.join(root_dir,
+                                                             SCRIPTS_DIR)):
+        for filename in filenames:
+            if filename in wanted_scripts:
+                scripts.append(os.path.join(dirpath, filename))
+    return scripts
+
+import distutils.command.install_data
+
+#Taken from http://wiki.python.org/moin/Distutils/Tutorial
+# Specializations of some distutils command classes
+## Code borrowed from wxPython's setup and config files
+## I am not 100% sure what's going on, but it works!
+class wx_smart_install_data(distutils.command.install_data.install_data):
+    """need to change self.install_dir to the actual library dir"""
+    def run(self):
+        install_cmd = self.get_finalized_command('install')
+        self.install_dir = getattr(install_cmd, 'install_lib')
+        return distutils.command.install_data.install_data.run(self)
+
+PACKAGE_DIR = 'franklin'
+SCRIPTS_DIR = 'scripts'
+
+#dependencies
+MODULES = {'Bio':        {'required': True},
+           'configobj':  {'required': True},
+           'psubprocess':{'msg':'no parallel processing will be possible'},
+           'pysam':      {'msg':'SNP calling will fail'},
+           'matplotlib': {'msg':'some statistics will fail'}
+           }
+
 # check module dependencies
 check_modules(MODULES)
 
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files, modules = [], [], []
 root_dir = os.path.dirname(__file__)
-pieces = fullsplit(root_dir)
-if pieces[-1] == '':
-    len_root_dir = len(pieces) - 1
-else:
-    len_root_dir = len(pieces)
-
-for dirpath, dirnames, filenames in os.walk(os.path.join(root_dir,
-                                                         PACKAGE_DIR)):
-    if '__init__.py' in filenames:
-        package = '.'.join(fullsplit(dirpath)[len_root_dir:])
-        packages.append(package)
-        for filename in os.listdir(dirpath):
-            if (filename.startswith('.') or filename.startswith('_') or
-                not filename.endswith('.py')):
-                continue
-            modules.append(package + '.' + filename)
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
+packages = _guess_packages(root_dir)
 wanted_scripts = ['backbone_analysis.py', 'seqio.py',
                   'backbone_create_project.py']
-scripts = []
-for dirpath, dirnames, filenames in os.walk(os.path.join(root_dir,
-                                                         SCRIPTS_DIR)):
-    for filename in filenames:
-        if filename in wanted_scripts:
-            scripts.append(os.path.join(dirpath, filename))
+scripts = _guess_scripts(root_dir, wanted_scripts)
+data_files = find_data_files('ext', '*')
 
-setup(
+dist = setup(
     # basic package data
     name         = "franklin",
     version      = franklin.__version__,
     author       ='Jose Blanca, Peio Ziarsolo',
-    author_email ='jblanca@btc.upv.es',
+    author_email ='jblanca@upv.es',
     description  ='Some genomics related classes',
     # package structure
     include_package_data = True,
     packages=packages,
     package_dir={'':'.'},
-    #py_modules = modules,
 
     package_data={'': ['data/samtools/*', 'data/blast/*', 'data/*.*',
-                       'data/samtools_color/*']},
-
-    requires=['BioPython', 'sqlalchemy', 'matplotlib', 'configobj',
+                       'data/samtools_color/*',
+                       ]},
+    data_files = data_files,
+    cmdclass = { 'install_data':    wx_smart_install_data },
+    requires=['BioPython', 'matplotlib', 'configobj',
               'pysam', 'psubprocess'],
     scripts=scripts,
 )
