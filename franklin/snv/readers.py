c22a7738e69b8e1b9f11dce81144f0b76481d7d9
Peio <pziarsolo@upv.es>
2011-11-21 12:49:54 +0100

    vcfparser added

---
+++ franklin/snv/readers.py
@@ -0,0 +1,117 @@
+'''
+Created on 2011 aza 21
+
+@author: peio
+'''
+class VcfParser(object):
+    'A vcf reader'
+    def __init__(self, fpath):
+        'Class initiator'
+        self._fpath = fpath
+        self.header = None
+        self._get_header()
+        self._index = None
+
+    def _get_version(self):
+        'version of the vcf'
+        version_unformat = self.header['format']
+        return version_unformat.split('v')[1]
+    version = property(_get_version)
+
+    def _get_header(self):
+        'it returns the header'
+        if self.header is not None:
+            return self.header
+        headers = {}
+        for line in open(self._fpath):
+            if not line.startswith('#'):
+                break
+            if line.startswith('##'):
+                line = line.strip()
+                line = line.lstrip('##')
+                kind, value = line.split('=', 1)
+                if kind == 'FILTER':
+                    if kind not in headers:
+                        headers[kind] = {}
+                    filter_type, filter_info = value.split(',', 1)
+                    headers[kind][filter_type] = filter_info.strip('"')
+                elif kind in ('FORMAT', 'INFO'):
+                    if kind not in headers:
+                        headers[kind] = {}
+                    name, example, type_, desc = value.split(',')
+                    headers[kind][name] = {'type':type_, 'example':example,
+                                           'description':desc}
+                else:
+                    headers[kind] = value
+            else:
+                line = line.lstrip('#')
+                headers['colnames'] = line.split()
+        self.header = headers
+
+    def _get_vcfs(self):
+        'vcf generator'
+        colnames = self.header['colnames']
+        for line in open(self._fpath):
+            if line.startswith('#'):
+                continue
+            yield self._parse_vcf_line(line, colnames)
+    vcfs = property(_get_vcfs)
+
+    def _parse_vcf_line(self, line, colnames):
+        '''It parses the cvf svn line'''
+        vcf_items = line.split()
+        vcf = dict(zip(colnames[:9], vcf_items[:9]))
+        # reformat FILTER
+        vcf['FILTER'] = vcf['FILTER'].split(';')
+        # REformat INFO
+        info = vcf['INFO']
+        vcf['INFO'] = {}
+        for info_ in info.split(';'):
+            info_key, info_value = info_.split('=')
+            vcf['INFO'][info_key] = info_value
+        # reformat FORMAT
+        format_string = vcf['FORMAT']
+        vcf['FORMAT'] = {}
+        for format_ in format_string.split(';'):
+            format_key, format_value = format_.split(':')
+            vcf['FORMAT'][format_key] = format_value
+
+
+        vcf['samples'] = {}
+        for samples in zip(colnames[9:], vcf_items[9:]):
+            allele_count = {}
+            alleles, values =  samples[1].split(':')
+            for index , allele in enumerate(alleles.split('|')):
+                allele = vcf['REF'] if allele == 0 else vcf['ALT']
+                allele_count[allele] = values[index]
+            vcf['samples'][samples[0]] = allele_count
+        return vcf
+
+    def _make_index(self):
+        '''it makes an index of the vcf file. It takes the vcf position
+        (chrom, position) as index'''
+        if self._index is not None:
+            return self._index
+        index = {}
+        fhand = open(self._fpath, 'rt')
+        rawline = 'filled'
+        while len(rawline) != 0:
+            prior_tell = fhand.tell()
+            rawline = fhand.readline()
+            if  rawline and rawline[0] == '#':
+                continue
+            index[tuple(rawline.split()[:2])] = prior_tell
+        self._index = index
+
+    def get_snv(self, position):
+        'It returns an snv giving it position'
+        colnames = self.header['colnames']
+        if self._index is None:
+            self._make_index()
+        fhand = open(self._fpath)
+        file_position = self._index[position]
+        fhand.seek(file_position)
+        return self._parse_vcf_line(fhand.readline(), colnames)
+
+
+
---
+++ test/snv/readers_test.py
@@ -0,0 +1,34 @@
+'''
+Created on 2011 aza 21
+
+@author: peio
+'''
+import unittest
+from os.path import join
+from franklin.utils.misc_utils import TEST_DATA_DIR
+from franklin.snv.readers import VcfParser
+
+class TestVcfParser(unittest.TestCase):
+
+
+    def test_vcfparser(self):
+        'It test the vcf arser'
+        vcf_path = join(TEST_DATA_DIR, 'contigs.vcf')
+        vcf = VcfParser(vcf_path)
+        assert vcf.version == '3.3'
+
+        header = vcf.header
+        filter_info = 'SNV is not a CAP detectable by the enzymes: cheap ones'
+        assert header['FILTER']['CEF'] == filter_info
+
+        vcf_ = vcf.get_snv(('CUTC000002', '79'))
+        assert vcf_['CHROM'] == 'CUTC000002'
+        assert vcf_['POS'] == '79'
+
+        vcfs = list(vcf.vcfs)
+        assert len(vcfs) == 14
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_vcfparser']
+    unittest.main()
